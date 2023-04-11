47f58162dca66b88fa4b30c48567e2f3361d20fe
Allana Larissa <larissaleonel98@gmail.com>
2023-04-11 12:35:14 -0300

    add getter/setter on mentoria anda curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso  curso1 = new Curso();
+        curso1.setTitulo("java");
+        curso1.setDescricao("curso de java");
+        curso1.setCargaHoraria(11);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("ts");
+        curso2.setDescricao("typescript curso");
+        curso2.setCargaHoraria(2020);
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setData(LocalDate.now());
+
     }
 }
